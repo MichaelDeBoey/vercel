f9e9b8d98ed8de1aeb5fbe1be2df0dcfa661bb40
Michaël De Boey <info@michaeldeboey.be>
2023-08-01 20:46:28 +0200

    [examples] Update `remix` template

--- examples/remix/app/routes/edge.tsx
+++ examples/remix/app/routes/edge.tsx
@@ -2,11 +2,14 @@ import type { V2_MetaFunction } from "@vercel/remix";
 
 export const config = { runtime: "edge" };
 
-export const meta: V2_MetaFunction = () => [{ title: "Remix@Edge | New Remix App" }];
+export const meta: V2_MetaFunction = () => [
+  { title: "Remix@Edge | New Remix App" },
+  { name: "description", content: "Welcome to Remix!" },
+];
 
 export default function Edge() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix@Edge</h1>
     </div>
   );
